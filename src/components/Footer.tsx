50a514e66ae06346b6460ec4335ece790613640e
Marcos Paulo <marcospaulosousa48@gmail.com>
2023-11-15 16:31:38 -0300

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <p>
+          Copyright © 2023 - Desenvolvido por Marcos Dev - Todos os direitos
+          reservados
+        </p>
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
